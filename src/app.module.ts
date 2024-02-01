eaac2cce7ac7f0a4bd8068acb8eb2cc8a5ab6b4a
AntonioAndradeGomes <gomesmax1997@gmail.com>
2024-01-31 23:05:09 -0300

    feat: insert UserEntity

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from './user/entities/user.entity';
 
 @Module({
   imports: [
@@ -15,6 +16,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       port: Number(process.env.DB_PORT),
       username: process.env.DB_USERNAME,
       type: 'postgres',
+      synchronize: true,
+      entities: [UserEntity],
     }),
     UserModule,
   ],

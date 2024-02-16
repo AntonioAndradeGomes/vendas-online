e75a465fd6ee0b1a65d148d890c18fd1aed76e39
AntonioAndradeGomes <gomesmax1997@gmail.com>
2024-02-15 23:21:37 -0300

    cache service

---
+++ src/cache/cache.service.ts
@@ -0,0 +1,23 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { CACHE_MANAGER, Cache } from '@nestjs/cache-manager';
+
+@Injectable()
+export class CacheService {
+  constructor(@Inject(CACHE_MANAGER) private cacheManager: Cache) {}
+
+  async getCache<T>(
+    key: string,
+    functionRequest: () => Promise<T>,
+  ): Promise<T> {
+    const allData: T = await this.cacheManager.get(key);
+    if (allData) {
+      return allData;
+    }
+
+    const data: T = await functionRequest();
+
+    await this.cacheManager.set(key, data);
+
+    return data;
+  }
+}

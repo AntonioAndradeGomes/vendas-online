1674559069e042b02a462d1b967694abe0ac670a
Antonio Andrade Gomes Júnior <gomesmax1997@gmail.com>
2024-03-10 20:40:55 -0300

    feat: insert unit tests in state and city

---
+++ src/city/__mocks__/city.mock.ts
@@ -0,0 +1,9 @@
+import { CityEntity } from '../entities/city.entity';
+
+export const cityMock: CityEntity = {
+  createdAt: new Date(),
+  id: 123,
+  name: 'Quebrangulo',
+  updatedAt: new Date(),
+  stateId: 2,
+};
